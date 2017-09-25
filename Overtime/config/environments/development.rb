e34880b8beb2272a47dc46a2a16820d2c58e33e8
XI PAN <xpan12344321@gmail.com>
2017-09-25 17:43:35 +0000

    create devise User

--- Overtime/config/environments/development.rb
+++ Overtime/config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+   config.action_mailer.default_url_options = { host: 'https://ruby-on-rails-advanced-xp336.c9users.io' }
 end
